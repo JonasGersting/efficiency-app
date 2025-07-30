f5b5d5e6b97ca2ea95c8e46edf133ecf1f7bc814
Jonas Gersting <johndoe@example.com>
2025-07-30 07:28:49 +0200

    pwa functionality added manually.

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			injectRegister: 'script',
-			outDir: 'static', // <<— das ist entscheidend!
-			manifest: {
-				name: 'Efficiency App',
-				short_name: 'Efficiency App',
-				description: 'A PWA for tracking your efficiency',
-				theme_color: '#000000',
-				icons: [
-					{
-						src: 'icons/pwa_icon.png', // Pfad relativ zum `static` Ordner
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'icons/pwa_icon.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			}
-		})
 	]
 });
